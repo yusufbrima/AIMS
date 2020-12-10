76ab0129ce3e5c2f47949b95baa408f906ef6e5f
Yusuf Brima <yusufbrima@gmail.com>
2020-12-10 12:47:02 +0200

    model comparison

---
+++ Lectures/Review Phase/Block 4/Statistical Regression With R/Lab/10122020/playground.R
@@ -0,0 +1,14 @@
+library(corrplot)
+data =  read.csv("../09122020/data_medical_charges.csv",header = T)
+data$region = factor(x = data$region,levels =(unique(data$region)), )
+
+model1 = lm(charges ~ age + bmi+ sex,data =  data)
+
+summary(model1)
+
+
+model2 = lm(charges ~ age + bmi+ sex + region,data =  data)
+
+summary(model2)
+
+
